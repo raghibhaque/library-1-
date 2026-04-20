c6c95b7cab8427f88d77ed274aa72b5f990700ed
Raghib Haque <raghib.haque.2007@gmail.com>
2026-04-20 15:27:22 +0100

    Added new test

--- library/src/test/java/monty/library/LibraryApplicationTests.java
+++ library/src/test/java/monty/library/LibraryApplicationTests.java
@@ -1,27 +1,30 @@
 package monty.library;
 
-import monty.library.kyle.book;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import monty.library.kyle.Book;
+
 
 @SpringBootTest
 class LibraryApplicationTests {
 
 	@Test
-	void testBookCreatedWithValidInput () {
-		book book = new book("Clean Code", "Robert C. Martin", 2008, 9780132350884.0);
+	public void testBookCreatedWithValidInput() {
+		Book book = new Book("Clean Code", "Robert C. Martin", 2008, 9780132350884.0);
 
 		assertEquals("Clean Code", book.getTitle());
 		assertEquals("Robert C. Martin", book.getAuthor());
 		assertEquals(2008, book.getYearofPublication());
 		assertEquals(9780132350884.0, book.getIsbn(), 0.0);
 	}
-
 	@Test
-	void testBookWithEmptyTitle(){
+	public void testBookWithBlankTitle(){
+		Book book = new Book("","Kyle Joyce" , 2001 , 5454367567523.0);
+
+		assertEquals("", book.getTitle());
 
 	}
 }
-
