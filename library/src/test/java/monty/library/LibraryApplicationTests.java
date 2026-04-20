8474233b48101482f537f5e10a42303333654068
Raghib Haque <raghib.haque.2007@gmail.com>
2026-04-20 15:20:40 +0100

    add tests

--- library/src/test/java/monty/library/LibraryApplicationTests.java
+++ library/src/test/java/monty/library/LibraryApplicationTests.java
@@ -1,24 +1,27 @@
 package monty.library;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import monty.library.kyle.book;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import monty.library.kyle.Book;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class LibraryApplicationTests {
 
 	@Test
-    public void testBookCreatedWithValidInput() {
-        Book book = new Book("Clean Code", "Robert C. Martin", 2008, 9780132350884.0);
+	void testBookCreatedWithValidInput () {
+		book book = new book("Clean Code", "Robert C. Martin", 2008, 9780132350884.0);
 
-        assertEquals("Clean Code", book.getTitle());
-        assertEquals("Robert C. Martin", book.getAuthor());
-        assertEquals(2008, book.getYearofPublication());
-        assertEquals(9780132350884.0, book.getIsbn(), 0.0);
-    }
+		assertEquals("Clean Code", book.getTitle());
+		assertEquals("Robert C. Martin", book.getAuthor());
+		assertEquals(2008, book.getYearofPublication());
+		assertEquals(9780132350884.0, book.getIsbn(), 0.0);
+	}
 
+	@Test
+	void testBookWithEmptyTitle(){
+
+	}
 }
+
