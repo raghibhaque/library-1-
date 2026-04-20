de622211aa199866b5ee376ba25ca0942172a252
Raghib Haque <raghib.haque.2007@gmail.com>
2026-04-20 14:47:29 +0100

    Add book file and main

--- library/src/main/java/monty/library/LibraryApplication.java
+++ library/src/main/java/monty/library/LibraryApplication.java
@@ -1,13 +1,33 @@
 package monty.library;
 
+import monty.library.kyle.book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
+		book book;
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Enter book title: ");
+			String title = sc.nextLine();
+			System.out.print("Enter book author: ");
+			String author = sc.nextLine();
+			System.out.print("Enter book year of publication: ");
+			int yearofPublication = sc.nextInt();
+			System.out.print("Enter book ISBN: ");
+			double isbn = sc.nextDouble();
+			book = new book(title, author, yearofPublication, isbn);
+		}
+		System.out.println("Book Details:");
+		System.out.println("Title: " + book.getTitle());
+		System.out.println("Author: " + book.getAuthor());
+		System.out.println("Year of Publication: " + book.getYearofPublication());
+		System.out.println("ISBN: " + book.getIsbn());
+
 	}
 
 }
---
+++ library/src/main/java/monty/library/kyle/book.java
@@ -0,0 +1,31 @@
+package monty.library.kyle;
+
+public class book {
+        private String title;
+        private String author;
+        private int yearofPublication;
+        private double isbn;
+
+        public book(String title, String author, int yearofPublication, double isbn) {
+            this.title = title;
+            this.author = author;
+            this.yearofPublication = yearofPublication;
+            this.isbn = isbn;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public int getYearofPublication() {
+            return yearofPublication;
+        }
+
+        public double getIsbn() {
+            return isbn;
+        }
+    }
